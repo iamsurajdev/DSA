792d9be2f7c72dca559d0a7271b673ab315d3fb8
iamsurajdev <surajbiswas367@gmail.com>
2026-02-28 21:07:15 +0530

    added comments for two sum

--- twoSum.js
+++ twoSum.js
@@ -2,16 +2,16 @@ const nums = [1, 2, 4];
 const target = 6;
 
 var twoSum = function(nums, target) {
-    const map = new Map();
+    const map = new Map(); // initiating hash map
 
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
+        const complement = target - nums[i]; // checking the remaining value of target
 
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+        if (map.has(complement)) { // checking is the hashmap has the remaining value or not
+            return [map.get(complement), i]; // return the remaining value index & current index
         }
 
-        map.set(nums[i], i);
+        map.set(nums[i], i); // setting the current value with the index
     }
 };
 
