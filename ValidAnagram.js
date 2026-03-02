0cdbd4d26f1ac2ee7b0d81c8d2ab575ce5d4ee60
iamsurajdev <surajbiswas367@gmail.com>
2026-03-03 01:49:39 +0530

    ValidAnagram - comments added and formatted

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -1,27 +1,27 @@
 const word1 = "raac";
 const word2 = "caat";
 
+var isAnagram = function (s, t) {
+  //checking if length is same
+  if (s.length !== t.length) {
+    return false;
+  }
 
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
+  const count = {}; // taking an empty object
+
+  for (const i of s) {
+    count[i] = (count[i] || 0) + 1; // storing the letter with the number of occurrence
+  }
+
+  for (const i of t) {
+    // checking if the letter exists or not
+    if (!count[i]) {
+      return false;
     }
+    count[i] = count[i] - 1; // if the letter exists then reduce the number of occurrence
+  }
 
-    const count = {}
-
-    for (const i of s) {
-        count[i] = (count[i] || 0 ) + 1;
-    }
-
-    for (const i of t) {
-        if (!count[i]) {
-            return false;
-        }
-        count[i] = count[i] - 1
-    }
-
-    return true;
+  return true;
 };
 
-
-console.log(isAnagram(word1, word2))
+console.log(isAnagram(word1, word2));
